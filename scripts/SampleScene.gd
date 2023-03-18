a4b042ff2d326c5850916856d2485006f44b9775
Jkasper <jkasper@student.42heilbronn.de>
2023-03-18 14:06:20 +0100

    played around with input here

--- scripts/SampleScene.gd
+++ scripts/SampleScene.gd
@@ -8,11 +8,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func _unhandled_key_input(event):
-	if event.as_text() == "Escape":
+	# check for exit game
+	if Input.is_key_pressed(KEY_ESCAPE):
 		var scene_handler = get_parent()
 		scene_handler.print_tree()
 		scene_handler.get_node("sampleGame").queue_free()
 		scene_handler.get_node("MainMenu").set_process(true)
+	# check for tile selection
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var tile_node = get_parent()
+		var tile_map = tile_node.get_node("terrain").get_class()
+		var tile_id =  tile_map.local_to_map(get_global_mouse_position())
+		print(tile_id)
